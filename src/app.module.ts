91ee58de051d4573b18f8287c21096177dcf35ae
Fran <franivan.j.c@gmail.com>
2023-06-03 19:38:21 -0600

    Moves the startup logic to the app service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { GlobalService } from './global.service';
+import { MealsModule } from './meals/meals.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CategoriesModule,
+    MealsModule,
+    HttpModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GlobalService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+
+import { GlobalService } from './global.service';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  constructor(
+    private readonly globalService: GlobalService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  async onModuleInit() {
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.get(
+          'https://www.themealdb.com/api/json/v1/1/categories.php',
+        ),
+      );
+      this.globalService.setCategories(data.categories);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
