a35e3ef11cd2a76194b91c3da823d1a47a51a3fa
Fran <franivan.j.c@gmail.com>
2023-06-04 04:39:29 -0600

    Fixes excecution order bug that allowed the prices to be set in a wrong hierarchy.

--- src/utils/prices.ts
+++ src/utils/prices.ts
@@ -1,35 +1,22 @@
 const globalMin = 10;
 const globalMax = 100;
 
-const lowerMiddle = generateRandom(globalMin, globalMax);
-const upperMiddle = generateRandom(lowerMiddle, globalMax);
-
 function generateRandom(min: number, max: number) {
-  return Math.round(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-// Avoid limit colisions.
-const lowerMiddleFixed =
-  lowerMiddle === globalMin
-    ? lowerMiddle + 1
-    : lowerMiddle === globalMax
-    ? lowerMiddle - 1
-    : lowerMiddle;
-const upperMiddleFixed =
-  upperMiddle === globalMax
-    ? upperMiddle - 1
-    : upperMiddle === lowerMiddleFixed
-    ? upperMiddle + 1
-    : upperMiddle;
+// Set middle limits and avoid their colisions.
+const lowerMiddle = generateRandom(globalMin, globalMax - 1);
+const upperMiddle = generateRandom(lowerMiddle + 1, globalMax);
 
 export function generatePrice(category: string) {
   switch (category) {
     case "Pork":
-      return generateRandom(globalMin, lowerMiddleFixed);
+      return generateRandom(globalMin, lowerMiddle);
     case "Beef":
-      return generateRandom(lowerMiddleFixed, upperMiddleFixed);
+      return generateRandom(lowerMiddle, upperMiddle);
     case "Lamb":
-      return generateRandom(upperMiddleFixed, globalMax);
+      return generateRandom(upperMiddle, globalMax);
     default:
       return generateRandom(globalMin, globalMax);
   }
