0734dd82dc7d998184dea8f56306512ed53eddff
Fran <franivan.j.c@gmail.com>
2023-06-07 02:56:05 -0600

    Adds stripe and payment configuration and resources.

---
+++ src/payment/payment.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from "@nestjs/common";
+
+import { PaymentService } from "./payment.service";
+import { PaymentResponse } from "src/interfaces/payment";
+
+@Controller("payment")
+export class PaymentController {
+  constructor(private readonly paymentService: PaymentService) {}
+
+  @Post()
+  generateSession(@Body() { ammount }): Promise<PaymentResponse> {
+    console.log(ammount);
+    return this.paymentService.generateSession(ammount);
+  }
+}
